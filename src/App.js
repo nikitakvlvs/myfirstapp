4ac81761abcd45db9d6cbc1420b8cc6410af1c79
Your Name <you@example.com>
2019-02-13 17:17:58 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PubNubReact from 'pubnub-react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.pubnub = new PubNubReact({
+      publishKey: 'pub-c-01024171-274e-4bc1-9082-a4a70ad4e6f0',
+      subscribeKey: 'sub-c-a67bca1c-2ff0-11e9-b681-be2e977db94e'
+    });
+
+    this.pubnub.init(this);
+
+
+    this.state = {
+      headerText: "hello world",
+      clickCount: 0,
+      isBulbOn: true
+    }
+  }
+
+  // componentWillMount() {
+  //   this.pubnub.subscribe({
+  //     channels: ['Default'],
+  //     withPresence: true
+  //   });
+
+  //   this.pubnub.getMessage('Default', (msg) => {
+  //     console.log(msg);
+  //   });
+
+  //   this.pubnub.getStatus((st) => {
+  //     this.pubnub.publish({
+  //       message: 'hello world from react',
+  //       channel: 'Default'
+  //     });
+  //   });
+  // }
+
   render() {
+    console.log("hello world");
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {this.state.headerText + ':' + this.state.clickCount}
           </p>
           <a
             className="App-link"
@@ -17,8 +57,19 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Yo B
           </a>
+          <Button
+            onClick={() => {
+              console.log("Buttone Pressed")
+              this.setState({
+                clickCount: this.state.clickCount + 1,
+              })
+            }}
+            variant="contained"
+            color="primary">
+            "Press ME!"
+           </Button>
         </header>
       </div>
     );
